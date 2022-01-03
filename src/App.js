f8b0a35b85f86f8d966b08a2adb595d4e98efd42
seakr1948 <seakr1948@gmail.com>
2022-01-02 18:11:34 -0800

    Added desired keyboard functionality

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import Header from './Components/Header';
 import KeyPad from './Components/KeyPad';
 import Screen from './Components/Screen';
@@ -7,39 +7,49 @@ import './Sass/App.scss'
 
 function App() {
 
-  
-  // useEffect(() => {
-  //   let screen = document.getElementById('screen');
-  //   screen.setSelectionRange(output.length, output.length);
-
-  //   let keys = document.getElementsByClassName('keypad');
-  //   // console.log(keys[0].children);
-
-  //  Method half works. Does click buttton but adds more than one key. Not sure why.
-
-  //   document.addEventListener('keyup', (e) => {
-  //     e.preventDefault();
-
-  //     if(e.repeat){
-  //       return
-  //     }
-
-  //     if (e.key === '7'){
-  //       keys[0].children[0].click();
-  //     }
-  //   })
-
-  // })
-  
-
   const [currentTheme, setCurrentTheme] = useState(0);
+  const [arithmetic, setArithmetic]  = useState('');
+  const [output, setOutput] = useState('0');
+  const [currentKey, setCurrentKey] = useState('');
+
   const themes = ['theme-1', 'theme-2', 'theme-3'];
   const bg = ['bg-1', 'bg-2', 'bg-3'];
-  const [arithmetic, setArithmetic]  = useState('');
-
-  const [output, setOutput] = useState('0');
-
+  
   document.body.className = bg[currentTheme];
+    
+  const ALLOWED_KEYS = ['7', '8', '9', 'Backspace',
+                        '4', '5', '6', '+',
+                        '1', '2', '3', '-',
+                        '.', '0', '/', '*',
+                        'Escape', 'Enter'];
+  useEffect(() => {
+      const keys = document.getElementsByClassName('key');
+        const onKeyDown = ({key}) => {
+          if (ALLOWED_KEYS.includes(key)){
+            const temp = ALLOWED_KEYS.indexOf(key);
+            keys[temp].click();
+            keys[temp].classList.add('key-down');
+            setCurrentKey(temp);
+          }
+          
+          
+        }
+
+        const onKeyUp = () => {
+          // console.log(currentKey);
+          keys[currentKey].classList.remove('key-down');
+        }
+
+        document.addEventListener('keydown', onKeyDown);
+        document.addEventListener('keyup', onKeyUp);
+
+        return () => {
+            document.removeEventListener('keydown', onKeyDown);
+            document.removeEventListener('keyup', onKeyUp);
+        }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [currentKey]);
+
   return (
     <div className={`App flex ${themes[currentTheme]}`}>
       <main className='calculator-container flex'>
